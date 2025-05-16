8bfa65adcadcb87e7f8aacba644830a90613375c
Rafael Juan Cardoso Martins <rafael.juan@codex1.com.br>
2025-05-16 20:30:29 +0000

    bloco soma

--- src/main/java/blockly/SomarValores.java
+++ src/main/java/blockly/SomarValores.java
@@ -18,15 +18,17 @@ public static final int TIMEOUT = 300;
  * @param x0
  *
  * @author Rafael Juan Cardoso Martins
- * @since 16/05/2025, 16:59:46
+ * @since 16/05/2025, 17:06:56
  *
  */
 public static Var Executar(@ParamMetaData(description = "x", id = "e9b95939") @RequestBody(required = false) Var x, @ParamMetaData(description = "x0", id = "0e03c1aa") Var x0) throws Exception {
  return new Callable<Var>() {
 
    public Var call() throws Exception {
-    return
-Var.valueOf(blockly.BlocoHC.somarValores(x.getTypedObject(double.class), x0.getTypedObject(double.class)));
+    cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
+    Var.valueOf("vars.Resultado"),
+    Var.valueOf(blockly.BlocoHC.somarValores(x.getTypedObject(double.class), x0.getTypedObject(double.class))));
+    return Var.VAR_NULL;
    }
  }.call();
 }
